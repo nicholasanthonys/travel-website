ba5495ef020f87ac541b8cfa789fbec5033da6aa
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2023-03-12 15:10:41 +0700

    feat: apply typescript

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react'
+import tes from '@/elements/Button/index'
+const LandingPage: FC = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
